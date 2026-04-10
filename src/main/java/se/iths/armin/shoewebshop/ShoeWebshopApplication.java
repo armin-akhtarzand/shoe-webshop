113ef0170a49bd0cfbeac1a3f384fb6dd987db14
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-04-10 10:29:40 +0200

    Initial project structure

---
+++ src/main/java/se/iths/armin/shoewebshop/ShoeWebshopApplication.java
@@ -0,0 +1,13 @@
+package se.iths.armin.shoewebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeWebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoeWebshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/armin/shoewebshop/ShoeWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.armin.shoewebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeWebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

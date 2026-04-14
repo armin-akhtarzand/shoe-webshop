e3d264e11c82af3fd1528cb1c7679f69d2a726a8
FredrikAlmen93 <fredrik.x.almen@gmail.com>
2026-04-14 15:09:07 +0200

    add fields and validation to Product entity

--- src/main/java/se/iths/armin/shoewebshop/entity/Product.java
+++ src/main/java/se/iths/armin/shoewebshop/entity/Product.java
@@ -1,4 +1,31 @@
 package se.iths.armin.shoewebshop.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @Positive
+    private double price;
+
+    @NotBlank
+    private String category;
+
+    @NotBlank
+    private String imageUrl;
 }

40bdc8144ae8aa9b48c1b130e23d2b80b05ec24b
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-04-14 15:36:04 +0200

    feat(CartItem): add fields & contructor  to CartItem

--- src/main/java/se/iths/armin/shoewebshop/cart/CartItem.java
+++ src/main/java/se/iths/armin/shoewebshop/cart/CartItem.java
@@ -1,4 +1,19 @@
 package se.iths.armin.shoewebshop.cart;
 
+
+import se.iths.armin.shoewebshop.entity.Product;
+
+import java.math.BigDecimal;
+
 public class CartItem {
+
+    private Product product;
+    private int quantity;
+    private BigDecimal price;
+
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+        this.price = product.getPrice();
+    }
 }

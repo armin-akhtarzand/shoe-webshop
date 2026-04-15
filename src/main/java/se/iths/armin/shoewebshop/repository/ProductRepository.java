3ac05c03d4b76de360d5ca92abb194a3e2484341
FredrikAlmen93 <fredrik.x.almen@gmail.com>
2026-04-15 14:01:03 +0200

    feat(Order): update find method to use username instead of appUserId

--- src/main/java/se/iths/armin/shoewebshop/repository/ProductRepository.java
+++ src/main/java/se/iths/armin/shoewebshop/repository/ProductRepository.java
@@ -8,6 +8,4 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findByCategory(String category);
-
-    List<Product> findByProductName(String productName);
 }

43f1767d7b4598202370f924e0f8b81d2dccd190
FredrikAlmen93 <fredrik.x.almen@gmail.com>
2026-04-19 12:43:34 +0200

    feat(ProductService): rename methods for clarity and consistency

--- src/main/java/se/iths/armin/shoewebshop/service/ProductService.java
+++ src/main/java/se/iths/armin/shoewebshop/service/ProductService.java
@@ -11,24 +11,28 @@ import java.util.Optional;
 public class ProductService {
     private final ProductRepository productRepository;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository)
+    {
         this.productRepository =  productRepository;
     }
-
-    public Product save(Product product) {
+    public Product createProduct(Product product)
+    {
         return productRepository.save(product);
     }
 
-    public List<Product> getAllProducts() {
+    public List<Product> getAllProducts()
+    {
         return productRepository.findAll();
     }
 
-    public Product findById(Long id) {
+    public Product findById(Long id)
+    {
         return productRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Product not found"));
     }
 
-    public List<Product> getProductsByCategory(String category) {
+    public List<Product> getProductsByCategory(String category)
+    {
         return productRepository.findByCategory(category);
     }
 }

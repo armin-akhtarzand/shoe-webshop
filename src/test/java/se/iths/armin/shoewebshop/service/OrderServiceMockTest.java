53582cf823c8bd98423de245e101a2b98f13049e
FredrikAlmen93 <fredrik.x.almen@gmail.com>
2026-04-20 21:31:34 +0200

    refactor(OrderServiceMockTest.java): remove unused Hamcrest assertion import

--- src/test/java/se/iths/armin/shoewebshop/service/OrderServiceMockTest.java
+++ src/test/java/se/iths/armin/shoewebshop/service/OrderServiceMockTest.java
@@ -16,7 +16,6 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
